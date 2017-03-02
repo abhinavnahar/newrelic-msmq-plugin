bdd8e97df9af3bd0c8ca8e2f2b4faae31ed4b625
abhinavnahar <abhinavnahar@gmail.com>
2017-03-02 12:56:42 -0800

    Fixing exe in subfolder
    1. changing guid.
    2. Fixing msbuild to create exr under subfolder fof zip

--- MSMQNewRelicPlugin/MSMQNewRelicPlugin/MSMQAgent.cs
+++ MSMQNewRelicPlugin/MSMQNewRelicPlugin/MSMQAgent.cs
@@ -9,7 +9,7 @@ namespace MSMQNewRelicPlugin
 {
     class MSMQAgent : Agent
     {
-        public override string Guid { get { return "com.truemark.msmqplugin"; } }
+        public override string Guid { get { return "com.truemark.plugin.msmq"; } }
 
         public override string Version { get { return "1.0.0"; } }
 
